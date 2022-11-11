e60f302d7d9321c68aa29dc80d0e49ad1b7b3c4b
Reginald Griffin II <74481504+ReggieRay210@users.noreply.github.com>
2022-11-10 18:50:57 -0600

    Create Temp_Convert.py

---
+++ Temp_Convert.py
@@ -0,0 +1,10 @@
+fahrenheit= float(input('Enter the current temperature in °F:'))
+convert_to_celsius = (fahrenheit-32)*(5/9)
+print(fahrenheit, "°F is equal to", convert_to_celsius, "°C")
+
+
+
+celsius = float(input('Enter the current temperature in °C:'))
+convert_to_fahrenheit = (celsius*(9/5))+32
+print(celsius, "°C is equal to", convert_to_fahrenheit, "°F")
+
